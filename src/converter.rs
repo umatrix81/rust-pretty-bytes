dd8e5d8ebb7cb7d8a38c57a7c5c096cf66ce369c
umatrix81 <umatrix81@gmail.com>
2023-03-22 15:01:54 +0300

    Update converter.rs

--- src/converter.rs
+++ src/converter.rs
@@ -7,7 +7,7 @@ pub fn convert(num: f64) -> String {
   if num < 1_f64 {
       return format!("{}{} {}", negative, num, "B");
   }
-  let delimiter = 1000_f64;
+  let delimiter = 1024_f64;
   let exponent = cmp::min((num.ln() / delimiter.ln()).floor() as i32, (units.len() - 1) as i32);
   let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
   let unit = units[exponent as usize];
